ac2035b07c775eecd7c22189314f9ad56e26192d
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-09-15 16:00:31 -0500

    Completed views excercise

--- src/main/java/edu/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/edu/codeup/codeupspringblog/controllers/PostController.java
@@ -1,23 +1,35 @@
 package edu.codeup.codeupspringblog.controllers;
 
+import edu.codeup.codeupspringblog.models.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/posts")
 public class PostController {
 
     @GetMapping("")
-    @ResponseBody
-    public String indexPage() {
-        return "posts index page";
+    public String indexPage(Model model) {
+        List<Post> posts = new ArrayList<>();
+        Post post1 = new Post("Amazing Title", "Cool description, kadjflkdajfds lkadjklafdjskjfd kldjfkadsjfkldj");
+        Post post2 = new Post("Great Title", "Awesome description, kldajkdlsjfad kadjfkl;adsjf adkladjkladsjf adldkjalkdsjfdksljf");
+
+        posts.add(post1);
+        posts.add(post2);
+
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
 
     @GetMapping("/{id}")
-    @ResponseBody
-    public String viewIndividualPost(@PathVariable long id) {
-        return String.format("view an individual post from id %s", id);
+    public String viewIndividualPost(@PathVariable long id, Model model) {
+        Post post1 = new Post("this is the title", "this is the description kdsj;kladjkldasjk;llkd lkajfkldasjlkfdj lakdjflkasdjli;fj alaskdjfklasdjf lakdjfkadjfkldjf ");
+        model.addAttribute("post1", post1);
+        return "posts/show";
     }
 
     @GetMapping("/create")
---
+++ src/main/java/edu/codeup/codeupspringblog/models/Post.java
@@ -0,0 +1,13 @@
+package edu.codeup.codeupspringblog.models;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    private String title;
+    private String body;
+}
