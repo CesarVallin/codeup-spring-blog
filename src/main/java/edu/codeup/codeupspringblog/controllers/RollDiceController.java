70ceb2674acfe6f7c6a4b95f5b3d4cad05d625ea
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-09-15 14:08:41 -0500

    completed first exercise

--- src/main/java/edu/codeup/codeupspringblog/controllers/RollDiceController.java
+++ src/main/java/edu/codeup/codeupspringblog/controllers/RollDiceController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class RollDiceController {
 
@@ -15,9 +18,31 @@ public class RollDiceController {
 
     @GetMapping("/roll-dice/{number}")
     public String rollDiceWithRandomNumber(@PathVariable int number, Model model) {
-        int randomNumber = (int) (Math.floor(Math.random() * 6) + 1);
+        int randomNumber1 = (int) (Math.floor(Math.random() * 6) + 1);
+        int randomNumber2 = (int) (Math.floor(Math.random() * 6) + 1);
+        int randomNumber3 = (int) (Math.floor(Math.random() * 6) + 1);
+        int randomNumber4 = (int) (Math.floor(Math.random() * 6) + 1);
+
+        List<Integer> diceRolls = new ArrayList<>();
+        diceRolls.add(randomNumber1);
+        diceRolls.add(randomNumber2);
+        diceRolls.add(randomNumber3);
+        diceRolls.add(randomNumber4);
+
+//        boolean match = number == randomNumber;
+//        model.addAttribute("match", match);
+        int matchCounter = 0;
+        for(int i = 0; i < diceRolls.size(); i++) {
+            if(diceRolls.get(i) == number) {
+                matchCounter ++;
+            }
+        }
+
+        model.addAttribute("matchCounter", matchCounter);
         model.addAttribute("guessedNumber", number);
-        model.addAttribute("randomNumber", randomNumber);
+        model.addAttribute("diceRolls", diceRolls);
+//        model.addAttribute("randomNumber", randomNumber);
+
         return "roll-dice";
     }
 }
