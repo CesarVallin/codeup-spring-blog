0ad33ecca53f07bbc796f349b594e4c1e751b3de
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-09-18 12:25:10 -0500

    ContactDao

---
+++ sql/contacts-seeder.sql
@@ -0,0 +1,9 @@
+USE blog_db;
+
+INSERT INTO contacts (email, name) VALUES
+                                       ('cesar@email', 'cesar'),
+                                       ('test1@email', 'test1'),
+                                       ('test2@email', 'test2'),
+                                       ('test3@email', 'test3'),
+                                       ('test4@email', 'test4');
+
